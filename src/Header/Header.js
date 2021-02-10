4020a83c3fa06e4ace1006cb639b6f50e3695378
ranarupesh <rupeshrana288@gamil.com>
2021-02-10 19:05:50 +0530

    created header and navbar

---
+++ src/Header/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import HomeIcon from "@material-ui/icons/Home";
+import FlashOnIcon from "@material-ui/icons/FlashOn";
+import LiveTvIcon from "@material-ui/icons/LiveTv";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import SearchIcon from "@material-ui/icons/Search";
+import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
+
+import "./Header.css";
+
+function Header() {
+  return (
+    <div class="header">
+      <div className="header__icons">
+        <div className="header__icon header__icon--active">
+          <HomeIcon />
+          <p>Home</p>
+        </div>
+        <div className="header__icon">
+          <FlashOnIcon />
+          <p>Trending</p>
+        </div>
+        <div className="header__icon">
+          <LiveTvIcon />
+          <p>Verified</p>
+        </div>
+        <div className="header__icon">
+          <VideoLibraryIcon />
+          <p>Collections</p>
+        </div>
+        <div className="header__icon">
+          <SearchIcon />
+          <p>Search</p>
+        </div>
+        <div className="header__icon">
+          <PersonOutlineIcon />
+          <p>Account</p>
+        </div>
+      </div>
+      <div className="header__logo">
+        <img
+          src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png?fit=1280%2C680g"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Navbar/Nav.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <h4>Trending</h4>
+      <h4>Top Rated</h4>
+      <h4>Action</h4>
+      <h4>Comedy</h4>
+      <h4>Horror</h4>
+      <h4>Romance</h4>
+      <h4>Mystery</h4>
+      <h4>Sci-fi</h4>
+      <h4>Western</h4>
+      <h4>Animation</h4>
+      <h4>Movie</h4>
+    </div>
+  );
+}
+
+export default Nav;
