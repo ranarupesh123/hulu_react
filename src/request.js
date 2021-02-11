4d18cf1940244dc90565250c4c630df5c0cbc608
ranarupesh <rupeshrana288@gamil.com>
2021-02-11 08:41:45 +0530

    addded environmental variable

--- src/request.js
+++ src/request.js
@@ -1,4 +1,4 @@
-const API_KEY = "cdf541caef5dc341ddb8c2a9106d5f9c";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 export default {
   fetchTrending: `https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`,
